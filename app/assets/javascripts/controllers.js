d1e803ecbad81c7fb6cc996b8360c540be5a66b0
Mikołaj Jakubowski <mikolaj.jakubowski@leonteq.com>
2016-12-15 17:13:39 +0100

    remove old search ui

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -3,7 +3,6 @@ define([
     'controllers/AddUserCtrl',
     'controllers/EditUserCtrl',
     'controllers/ListUserCtrl',
-    'controllers/SearchCtrl',
     'controllers/RunCtrl'
 ], function () {
 
